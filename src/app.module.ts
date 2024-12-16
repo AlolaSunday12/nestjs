3d4eba6b44b927adaa3632393dc9db2d1fae12da
AlolaSunday12 <alolasj@gmail.com>
2024-12-16 15:53:37 +0100

    book route update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URL),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
