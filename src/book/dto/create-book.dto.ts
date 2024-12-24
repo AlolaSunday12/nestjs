5ab95f401ee67c8537f765fb549999fb9576dc0d
AlolaSunday12 <alolasj@gmail.com>
2024-12-24 23:37:04 +0100

    update on create-book-dto

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,9 +1,27 @@
-import { Category } from 'src/schemas/book.schema';
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Category } from '../schemas/book.schema';
+//import { BookSchema } from '../schemas/book.schema';
+
 
 export class CreateBookDto {
+
+  @IsNotEmpty()
+  @IsString()
   readonly title: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly author: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   readonly price: number;
+
+  @IsNotEmpty()
+  @IsEnum(Category, { message: 'please type correct Category.' })
   readonly category: Category;
 }
