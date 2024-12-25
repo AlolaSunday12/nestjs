105e09b85f32f27eabad6abd73a65457e2ba644d
AlolaSunday12 <alolasj@gmail.com>
2024-12-25 14:39:20 +0100

    update on book dto

--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,9 +1,23 @@
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 import { Category } from '../schemas/book.schema';
 
 export class UpdateBookDto {
+  @IsOptional()
+  @IsString()
   readonly title: string;
+
+  @IsOptional()
+  @IsString()
   readonly description: string;
+
+  @IsOptional()
+  @IsString()
   readonly author: string;
+
+  @IsOptional()
+  @IsNumber()
   readonly price: number;
+
+  @IsEnum(Category, { message: 'please type correct Category.' })
   readonly category: Category;
 }
