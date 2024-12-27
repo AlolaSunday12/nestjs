8f3b5de66af56dd7868239869defd15b7acd4589
AlolaSunday12 <alolasj@gmail.com>
2024-12-27 23:00:21 +0100

    jsonwebtoken strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,26 +5,25 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
 
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectModel(User.name)
-    private userModel: Model<User>
+    private userModel: Model<User>,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET
-    })
+      secretOrKey: process.env.JWT_SECRET,
+    });
   }
 
   async validate(payload) {
     const { id } = payload;
 
-    const user = await this.userModel.findById(id)
+    const user = await this.userModel.findById(id);
 
     if (!user) {
-      throw new UnauthorizedException('Login first to access this endpoint')
+      throw new UnauthorizedException('Login first to access this endpoint');
     }
     return user;
   }
