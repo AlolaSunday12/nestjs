1c6e0e191244a5fee4b1cdd0869a7b2acf50feea
AlolaSunday12 <alolasj@gmail.com>
2025-01-01 20:14:01 +0100

    update on multer and images

---
+++ src/multer.config.ts
@@ -0,0 +1,22 @@
+import { diskStorage } from 'multer';
+
+export const multerOptions = {
+  storage: diskStorage({
+    destination: './uploads',
+    filename: (req, file, cb) => {
+      const name = file.originalname.split('.')[0];
+      const fileExtension = file.originalname.split('.').pop();
+      const newFileName = `${name.replace(/\s/g, '-')}-${Date.now()}.${fileExtension}`;
+      cb(null, newFileName);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(
+        new Error('Invalid file type. Only image files are allowed.'),
+        false
+      )
+    }
+    cb(null, true);
+  },
+};
