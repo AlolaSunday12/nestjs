1313eb6008eeca8613bb76ec2b8e478fcec8c215
AlolaSunday12 <alolasj@gmail.com>
2025-01-03 22:54:42 +0100

    update on multer config

--- src/multer.config.ts
+++ src/multer.config.ts
@@ -1,9 +1,24 @@
-export async function uploadImagesToMongo(files: Array<Express.Multer.File>) {
-  const images = files.map((file) => ({
-    originalname: file.originalname,
-    mimetype: file.mimetype,
-    data: file.buffer, // Store as Binary data
-  }));
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
-  return images; // This will be saved directly to MongoDB in the `book` document
-}
+// Configure FilesInterceptor
+@UseInterceptors(
+  FilesInterceptor('files', 10, {
+    storage: diskStorage({
+      destination: './uploads', // Save files to the 'uploads' folder
+      
+      filename: (req, file, callback) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const ext = extname(file.originalname);
+        callback(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+      },
+    }),
+    fileFilter: (req, file, callback) => {
+      if (!/(jpg|jpeg|png)$/.test(file.mimetype)) {
+        return callback(new Error('Only image files are allowed!'), false);
+      }
+      callback(null, true);
+    },
+  }),
+);
