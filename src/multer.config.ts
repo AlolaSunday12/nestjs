4969a6409814e2d02386b6239efabc6d52166ecb
AlolaSunday12 <alolasj@gmail.com>
2025-01-03 12:38:46 +0100

    update on multer config

--- src/multer.config.ts
+++ src/multer.config.ts
@@ -1,23 +1,9 @@
-import { diskStorage } from 'multer';
+export async function uploadImagesToMongo(files: Array<Express.Multer.File>) {
+  const images = files.map((file) => ({
+    originalname: file.originalname,
+    mimetype: file.mimetype,
+    data: file.buffer, // Store as Binary data
+  }));
 
-// multer configuration
-export const multerOptions = {
-  storage: diskStorage({
-    destination: './uploads',
-    filename: (req, file, cb) => {
-      const name = file.originalname.split('.')[0];
-      const fileExtension = file.originalname.split('.').pop();
-      const newFileName = `${name.replace(/\s/g, '-')}-${Date.now()}.${fileExtension}`;
-      cb(null, newFileName);
-    },
-  }),
-  fileFilter: (req, file, cb) => {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(
-        new Error('Invalid file type. Only image files are allowed.'),
-        false,
-      );
-    }
-    cb(null, true);
-  },
-};
+  return images; // This will be saved directly to MongoDB in the `book` document
+}
