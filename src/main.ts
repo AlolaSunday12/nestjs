f1641908d8992d957767897dcee5d91ae5c09c70
AlolaSunday12 <alolasj@gmail.com>
2025-01-06 21:18:35 +0100

    update on main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
 import { join } from 'path';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
+  app.enableCors({
+    origin: ['http://localhost:5000'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.use('/uploads', express.static(join(__dirname, '..', 'public/uploads')));
   await app.listen(process.env.PORT ?? 3000);
